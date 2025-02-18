21355afd9abd97c3113b5c19952c21f08f9743ba
etamisie <32704830+ElsaTam@users.noreply.github.com>
2025-02-18 18:51:30 -0500

    Get background color with canvas

--- src/helpers/graph.ts
+++ src/helpers/graph.ts
@@ -1,5 +1,6 @@
 import { WorkspaceLeaf } from "obsidian";
 import { GraphEngine, GraphRenderer, GraphView, LocalGraphView } from "obsidian-typings";
+import { PluginInstances } from "src/pluginInstances";
 
 export function getBackgroundColor(renderer: GraphRenderer): Uint8Array {
     let bg = window.getComputedStyle(renderer.interactiveEl).backgroundColor;
@@ -8,9 +9,36 @@ export function getBackgroundColor(renderer: GraphRenderer): Uint8Array {
         el = el.parentElement as Element;
         bg = window.getComputedStyle(el).backgroundColor;
     }
-    bg = bg.replace("rgba", "").replace("rgb", "").replace("(", "").replace(")", "");
-    const RGB = bg.split(", ").map(c => parseInt(c));
-    return Uint8Array.from(RGB);
+
+    const canvas = createEl('canvas');
+    canvas.width = canvas.height = 1;
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+        try {
+            ctx.clearRect(0, 0, 1, 1);
+            ctx.fillStyle = bg;
+            ctx.fillRect(0, 0, 1, 1);
+            const RGBA = [ ... ctx.getImageData(0, 0, 1, 1).data ];
+            return new Uint8Array([RGBA[0], RGBA[1], RGBA[2]]);
+        }
+        catch {
+
+        }
+    }
+
+    try {
+        bg = bg.replace("rgba", "").replace("rgb", "").replace("(", "").replace(")", "");
+        const RGB = bg.split(", ").map(c => parseInt(c));
+        return Uint8Array.from(RGB);
+    }
+    finally {
+        if (PluginInstances.app.vault.getConfig('theme') === "moonstone ") {
+            return new Uint8Array([255, 255, 255]);
+        }
+        else {
+            return new Uint8Array([0, 0, 0]);
+        }
+    }
 }
 
 export function getGraphView(leaf: WorkspaceLeaf): GraphView | LocalGraphView | undefined {
